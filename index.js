25d27e5754c03ab54d9c18ba94a0f720d131427d
Dan <djthackray@gmail.com>
2022-12-10 20:38:27 +0000

    Add click listener to HTML document to close dropdown

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ dropdownBtn.addEventListener("click", function (e) {
     e.stopPropagation()
     toggleDropdown()
 })
+
+document.documentElement.addEventListener("click", function () {
+        if (dropdownMenu.classList.contains("show")) {
+            toggleDropdown();
+        }
+    })
