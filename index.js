660deeb4cc8000d160392ad6e04505ce31247b87
Dan <djthackray@gmail.com>
2022-12-10 18:55:56 +0000

    JS not working correctly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const dropdownBtn = document.getElementById("btn");
-const dropdownMenu = document.getElementById("dropdown");
-const toggleArrow = document.getElementById("arrow");
+const dropdownBtn = document.getElementById("btn")
+const dropdownMenu = document.getElementById("dropdown")
+const toggleArrow = document.getElementById("arrow")
 
 const toggleDropdown = function () {
     dropdownMenu.classList.toggle("show")
